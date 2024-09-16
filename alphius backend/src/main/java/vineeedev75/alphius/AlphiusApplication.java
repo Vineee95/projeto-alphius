e8acf72cb7cb507363b69c302a84ceddb17af11b
Vineee75 <viniciusdev75@gmail.com>
2024-09-15 21:57:46 -0300

    Início do Projeto Alphius.

---
+++ alphius backend/src/main/java/vineeedev75/alphius/AlphiusApplication.java
@@ -0,0 +1,13 @@
+package vineeedev75.alphius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphiusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlphiusApplication.class, args);
+	}
+
+}
---
+++ alphius backend/src/test/java/vineeedev75/alphius/AlphiusApplicationTests.java
@@ -0,0 +1,13 @@
+package vineeedev75.alphius;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlphiusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
